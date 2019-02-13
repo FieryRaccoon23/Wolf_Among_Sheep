100adf27ae6fa1b1acf1387714ca176ac6ddf9cb
FieryRaccoon23 <araib.saleem@gmail.com>
2019-02-12 19:15:56 -0500

    Manager and Loader
    - Added a game manager script (note, all other managers will go here e.g. Network/Online Manager)
    - Loader Script added, to load the managers
    
    (Note: Only the scripts are added at the moment. The game object (prefab) for them has not yet been added)

---
+++ Wolf Among Sheep/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        //Check if instance already exists, if not, set instance to this
+        if (instance == null)
+        {
+            instance = this;
+        }
+        //If instance already exists and it's not this, destroy
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+    }
+}
